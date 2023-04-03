9247b035a94f795d0c7501eb48f543a2a06cf895
Alex <alebar03@ucm.es>
2023-04-03 17:19:20 +0200

    Cada vez que la bola rebota con las palas se suma uno al marcador, y ahora que la bola solo se mueve horizontalmente

---
+++ basic.py
@@ -0,0 +1,303 @@
+import pygame
+import sys, os
+import socket
+import pickle
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+X = 0
+Y = 1
+SIZE = (700, 525)
+
+LEFT_PLAYER = 0
+RIGHT_PLAYER = 1
+PLAYER_COLOR = [GREEN, YELLOW]
+PLAYER_HEIGHT = 70
+PLAYER_WIDTH = 10
+
+BALL_COLOR = WHITE
+BALL_SIZE = 10
+FPS = 60
+DELTA = 30
+
+SIDES = ["left", "right"]
+
+class Player():
+    def __init__(self, side):
+        self.side = side
+        if side == LEFT_PLAYER:
+            self.pos = [10, SIZE[Y]//2]
+        else:
+            self.pos = [SIZE[X] - 10, SIZE[Y]//2]
+
+    def get_pos(self):
+        return self.pos
+
+    def get_side(self):
+        return self.side
+
+    def moveDown(self):
+        self.pos[Y] += DELTA
+        if self.pos[Y] > SIZE[Y]:
+            self.pos[Y] = SIZE[Y]
+
+    def moveUp(self):
+        self.pos[Y] -= DELTA
+        if self.pos[Y] < 0:
+            self.pos[Y] = 0
+    
+    def dispara(self):
+        pass
+
+    def __str__(self):
+        return f"P<{SIDES[self.side], self.pos}>"
+
+class Ball():
+    def __init__(self, velocity):
+        self.pos=[ SIZE[X]//2, SIZE[Y]//2 ]
+        self.velocity = velocity
+
+    def get_pos(self):
+        return self.pos
+
+    def update(self):
+        self.pos[X] += self.velocity[X]
+        #self.pos[Y] += self.velocity[Y]
+
+    def bounce(self, AXIS):#vamos
+        self.velocity[AXIS] = -self.velocity[AXIS]
+
+    def collide_player(self):
+        self.bounce(X)
+        print('ahiiiii')
+        for i in range(3):
+            self.update()
+
+    def __str__(self):
+        return f"B<{self.pos}>"
+
+
+class Game():
+    def __init__(self):
+        self.players = [Player(i) for i in range(2)]
+        self.ball = Ball([-2,3])
+        self.score = [0,0]
+        self.running = True
+    
+    def dispara(self,player):
+        self.players[player].dispara()
+
+    def get_player(self, side):
+        return self.players[side]
+
+    def get_ball(self):
+        return self.ball
+
+    def get_score(self):
+        return self.score
+
+    def is_running(self):
+        return self.running
+
+    def stop(self):
+        self.running = False
+
+    def moveUp(self, player):
+        self.players[player].moveUp()
+
+    def moveDown(self, player):
+        self.players[player].moveDown()
+
+    def movements(self):
+        self.ball.update()
+        pos = self.ball.get_pos()
+        if pos[Y]<0 or pos[Y]>SIZE[Y]:
+            self.ball.bounce(Y)
+
+    def __str__(self):
+        return f"G<{self.players[RIGHT_PLAYER]}:{self.players[LEFT_PLAYER]}:{self.ball}>"
+
+'''
+def movements(self):
+    self.ball.update()
+    pos = self.ball.get_pos()
+    posicion_left=self.players[0].get_pos()
+    posicion_right=self.players[1].get_pos()
+    if pos[Y]<0 or pos[Y]>SIZE[Y]:
+        self.ball.bounce(Y)
+    if pos[X]==posicion_left[X] and pos[Y]==posicion_left[Y]:
+        self.score[LEFT_PLAYER] += 1
+        #self.ball.bounce(X)
+    elif pos[X]==posicion_right[X] and pos[Y]==posicion_right[Y]:
+        self.score[RIGHT_PLAYER] += 1
+       # self.ball.bounce(X)
+       
+    def movements(self):
+        self.ball.update()
+        pos = self.ball.get_pos()
+        posicion_left=self.players[0].get_pos()
+        posicion_right=self.players[1].get_pos()
+        if pos[Y]<0 or pos[Y]>SIZE[Y]:
+            self.ball.bounce(Y)
+        if posicion_left[X]+(PLAYER_WIDTH)<=pos[X]<=posicion_left[X]-(PLAYER_WIDTH) and posicion_left[Y]+(PLAYER_HEIGHT)<=pos[X]<=posicion_left[Y]-(PLAYER_HEIGHT):
+            self.score[LEFT_PLAYER] += 1
+            #self.ball.bounce(X)
+        elif posicion_right[X]+(PLAYER_WIDTH)<=pos[X]<=posicion_right[X]-(PLAYER_WIDTH) and posicion_right[Y]+(PLAYER_HEIGHT)<=pos[X]<=posicion_right[Y]-(PLAYER_HEIGHT):
+            self.score[RIGHT_PLAYER] += 1
+           # self.ball.bounce(X)       
+def movements(self):
+    self.ball.update()
+    pos = self.ball.get_pos()
+    if pos[Y]<0 or pos[Y]>SIZE[Y]:
+        self.ball.bounce(Y)
+    if pos[X]>SIZE[X]:
+        self.score[LEFT_PLAYER] += 1
+        self.ball.bounce(X)
+    elif pos[X]<0:
+        self.score[RIGHT_PLAYER] += 1
+        self.ball.bounce(X)
+'''
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, player):
+      super().__init__()
+      self.image = pygame.Surface([PLAYER_WIDTH, PLAYER_HEIGHT])
+      self.image.fill(BLACK)
+      self.image.set_colorkey(BLACK)#drawing the paddle
+      self.player = player
+      color = PLAYER_COLOR[self.player.get_side()]
+      pygame.draw.rect(self.image, color, [0,0,PLAYER_WIDTH, PLAYER_HEIGHT])
+      self.rect = self.image.get_rect()
+      self.update()
+
+    def update(self):
+        pos = self.player.get_pos()
+        self.rect.centerx, self.rect.centery = pos
+
+    def __str__(self):
+        return f"S<{self.player}>"
+
+
+class BallSprite(pygame.sprite.Sprite):
+    def __init__(self, ball):
+        super().__init__()
+        self.ball = ball
+        self.image = pygame.Surface((BALL_SIZE, BALL_SIZE))
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, BALL_COLOR, [0, 0, BALL_SIZE, BALL_SIZE])
+        self.rect = self.image.get_rect()
+        self.update()
+
+    def update(self):
+        pos = self.ball.get_pos()
+        self.rect.centerx, self.rect.centery = pos
+
+
+
+class Display():
+    def __init__(self, game):
+        self.game = game
+        self.paddles = [Paddle(self.game.get_player(i)) for i in range(2)]
+
+        self.ball = BallSprite(self.game.get_ball())
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_group = pygame.sprite.Group()
+        for paddle  in self.paddles:
+            self.all_sprites.add(paddle)
+            self.paddle_group.add(paddle)
+        self.all_sprites.add(self.ball)
+
+        self.screen = pygame.display.set_mode(SIZE)
+        self.clock =  pygame.time.Clock()  #FPS
+        self.background = pygame.image.load('background.png')
+        running = True
+        pygame.init()
+
+    def analyze_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.game.stop()
+                elif event.key == pygame.K_s:
+                    self.game.moveUp(LEFT_PLAYER)
+                elif event.key == pygame.K_x:
+                    self.game.moveDown(LEFT_PLAYER)
+                elif event.key == pygame.K_k:
+                    self.game.moveUp(RIGHT_PLAYER)
+                elif event.key == pygame.K_m:
+                    self.game.moveDown(RIGHT_PLAYER)
+                elif event.key == pygame.K_d:
+                    self.game.dispara(LEFT_PLAYER)
+                elif event.key == pygame.K_j:
+                    self.game.dispara(RIGHT_PLAYER)
+        if pygame.sprite.spritecollide(self.ball, self.paddle_group, False):
+           if self.ball.ball.get_pos()[0]>500:
+              self.game.score[LEFT_PLAYER]+=1
+           else:
+              self.game.score[LEFT_PLAYER]+=1
+           self.game.get_ball().collide_player()
+        self.all_sprites.update()
+
+
+
+    def refresh(self):
+        self.screen.blit(self.background, (0, 0))
+        score = self.game.get_score()
+        font = pygame.font.Font(None, 74)
+        text = font.render(f"{score[LEFT_PLAYER]}", 1, WHITE)
+        self.screen.blit(text, (250, 10))
+        text = font.render(f"{score[RIGHT_PLAYER]}", 1, WHITE)
+        self.screen.blit(text, (SIZE[X]-250, 10))
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+    def tick(self):
+        self.clock.tick(FPS)
+
+    @staticmethod
+    def quit():
+        pygame.quit()
+
+class Network:
+    def __init__(self): ##this will connect to the server initially
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = '127.0.0.1' #server ip #<---
+        self.port = 5555   #server port #<---
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+    def getP(self):
+        return self.p
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return pickle.loads(self.client.recv(2048))
+        except:
+            pass
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as e:
+            print(e)
+
+
+
+def main():
+    try:
+        game = Game()
+        display = Display(game)
+
+        while game.is_running():
+            game.movements()
+            display.analyze_events()
+            display.refresh()
+            display.tick()
+    finally:
+        pygame.quit()
+
+if __name__=="__main__":
+    main()
